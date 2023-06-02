6a6e51518610459a479a9b2c7be48dd2a842699b
nova <yishaigame999@gmail.com>
2023-06-02 14:48:43 +0300

    add the sending comment in the blog feature

--- blog.php
+++ blog.php
@@ -10,47 +10,52 @@ include_once('header.php');
         include_once('./blog/msg_mgt.php');
         ?>
     </main>
-    <!-- <section class="user_msg_box">
+    <section class="user_msg_box">
         <h2 class="subheader">תגובות</h2>
         <form action="blog.php" method="post" class="user_form">
             <input type="text" name="title" class="user_inputs" placeholder="title">
             <textarea name="message" class="user_inputs text_input" placeholder="Message goes in here"></textarea>
             <input type="submit" value="send" name="submit" class="user_inputs">
         </form>
+        <div class="comment_container">
             <?php
-                // if (isset($_POST["submit"])) {
-                //     if ($_POST["submit"]) {
-                //         if (!empty($_POST["message"]) && !empty($_POST["title"])) {
-                //             $json_file = file_get_contents('./blog/comments.json');
-                //             $comnt_json = json_decode($json_file,true);
-                //             $comnt_json[] = ['title' => $_POST['title'], 'content'=> $_POST['message']];
-                //             $json_string = json_encode($data, JSON_PRETTY_PRINT);
-                //             file_put_contents('./blog/comments.json',$json_string);
-                //         } else {
-                //             header("Location: ../blog.php");
-                //             $status = false;
-                //         }
-                //     }
-                // }
-                // $json_file = file_get_contents('./blog/comments.json');
-                // $comnt_json = json_decode($json_file,true);
-                // for($i = 0; $i < count($comnt_json); $i++){
-                //     $jsonObj = $comnt_json[$i];
-                //     $item = '
-                //     <div class="msg_content comment">
-                //     <div class="from">
+            
+                if (isset($_POST["submit"])) {
+                    if ($_POST["submit"]) {
+                        if (!empty($_POST["message"]) && !empty($_POST["title"])) {
+                            $json_file = file_get_contents('./blog/comments.json');
+                            $comnt_json = json_decode($json_file,true);
+                            $comnt_json[] = ['title' => $_POST['title'],'date' => date("d/m/Y"), 'content'=> $_POST['message']];
+                            $json_string = json_encode($comnt_json, JSON_PRETTY_PRINT);
+                            file_put_contents('blog/comments.json',$json_string);
+                        } else {
+                            header("Location: blog.php");
+                            $status = false;
+                        }
+                        header('Location: ' . $_SERVER['HTTP_REFERER']);
+                        die();
+                    }
+                }
+                $json_print_file = file_get_contents('./blog/comments.json');
+                $comnt_print_file_json = json_decode($json_print_file,true);
+                for($i = 0; $i < count($comnt_print_file_json); $i++){
+                    $jsonObj = $comnt_print_file_json[$i];
+                    $item = '
+                    <div class="msg_content comment">
+                    <div class="from">
                     
-                //     </div>
-                //     <h4>' . $jsonObj["date"] . '</h4>
-                //         <h2>' . $jsonObj["title"] . '</h2>
+                    </div>
+                    <h4>' . $jsonObj["date"] . '</h4>
+                        <h2>' . $jsonObj["title"] . '</h2>
                         
-                //          <p>' . $jsonObj["content"] . '</p>
-                //     </div>
-                //     ';
-                //     echo $item;
-                // }
+                         <p>' . $jsonObj["content"] . '</p>
+                    </div>
+                    ';
+                    echo $item;
+                }
             ?>
-    </section> -->
+        </div>
+    </section>
 </div>
 <?php
 include_once('footer.php');
--- course.php
+++ course.php
@@ -6,49 +6,53 @@
 <main class="course_container">
     <div class="videolist_container">
         <div class="course_video_container">
-        <video class="video_box" controls>
-            <source src="movie.mp4" type="video/mp4">
-            <source src="movie.ogg" type="video/ogg">
-            Your browser does not support the video tag.
-        </video>
+                <iframe class="video_box" src="https://drive.google.com/file/d/16D_c9RJH0SlHDjOhPriEtyRZ_QnZFS6M/preview" width="640" height="480" allow="autoplay"></iframe>
         </div>
         <div class="playlist_videos_container">
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
-            <img src="./imgs/pic.jpg" alt="not found :(">
-            <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div class="course_list_item" >
+                <img src="./imgs/pic.jpg" alt="not found :(">
+                <p class="descpt">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
         </div>
     </div>
     <div class="exercises">
